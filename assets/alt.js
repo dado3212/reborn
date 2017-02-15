d96464f6ac409ad55df8d855255a6ce49760daf8
Alex Beals <alexcbeals@gmail.com>
2017-02-14 23:57:26 -0800

    respawning working, but goodbye matter.js
    Matter.JS is garbage.  The way it handles rotation and position translations into velocity makes it hard to customize, and thus a terrible choice for a platformer.  Better for physics simulations, I would think.

--- assets/alt.js
+++ assets/alt.js
@@ -4,7 +4,16 @@ var Engine = Matter.Engine,
   Render = Matter.Render,
   World = Matter.World,
   Events = Matter.Events,
-  Bodies = Matter.Bodies;
+  Bodies = Matter.Bodies,
+  Body = Matter.Body;
+
+// Key constants for readability
+const KEY_UP = 38;
+const KEY_LEFT = 37;
+const KEY_RIGHT = 39;
+const KEY_SUICIDE = 88; // X
+const KEY_Z = 90;
+const KEY_CORPOREAL = 67; // C
 
 // create an engine
 var engine = Engine.create();
@@ -19,119 +28,212 @@ var render = Render.create({
     background: '#222',
     wireframeBackground: '#222',
     enabled: true,
-    wireframes: true,
+    wireframes: false,
   }
 });
 
 //add the walls
+var wallOptions = {
+  isStatic: true,
+  render: {
+    fillStyle: '#222',
+    strokeStyle: 'white',
+    lineWidth: 1,
+  },
+}
 var offset = 5;
 World.add(engine.world, [
-  Bodies.rectangle(400, -offset, 800 + 2 * offset, 50, {
-    isStatic: true
-  }),
-  Bodies.rectangle(400, 600 + offset, 800 + 2 * offset, 50, {
-    isStatic: true
-  }),
-  Bodies.rectangle(800 + offset, 300, 50, 600 + 2 * offset, {
-    isStatic: true
-  }),
-  Bodies.rectangle(-offset, 300, 50, 600 + 2 * offset, {
-    isStatic: true
-  })
+  Bodies.rectangle(400, -offset, 800 + 2 * offset, 50, wallOptions),
+  Bodies.rectangle(400, 600 + offset, 800 + 2 * offset, 50, wallOptions),
+  Bodies.rectangle(800 + offset, 300, 50, 600 + 2 * offset, wallOptions),
+  Bodies.rectangle(-offset, 300, 50, 600 + 2 * offset, wallOptions)
 ]);
 
 // add some ramps to the world for the bodies to roll down
 World.add(engine.world, [
   Bodies.rectangle(340, 580, 700, 20, {
     isStatic: true,
-    angle: Math.PI * 0.06
+    angle: Math.PI * 0.06,
+    render: {
+      fillStyle: '#222',
+      strokeStyle: 'white',
+      lineWidth: 1,
+    }
   })
 ]);
 
-//add the player
-var player = Bodies.rectangle(600, 520, 25, 25, {
-  density: 0.05,
-  friction: 5,
-  frictionStatic: 0.2,
-  frictionAir: 0,
-  restitution: 0,
-  ground: false,
-  inertia: Infinity,
-});
+// Player creation function
+function createPlayer(spawn) {
+  return Bodies.rectangle(spawn.x, spawn.y, 25, 25, {
+    density: 0.05,
+    friction: 0.1,
+    frictionStatic: 0.1,
+    frictionAir: 0,
+    restitution: 0,
+    ground: false,
+    inertia: Infinity,
+    render: {
+      fillStyle: '#ddd',
+      strokeStyle: 'white',
+      lineWidth: 1,
+    },
+    // Custom parameters
+    spawn: spawn,
+    // Replay parameters
+    corporeal: true,
+    active: true,
+    moveList: [],
+  });
+}
 
+// Set up the world
 engine.world.gravity.y = 3.3;
 engine.world.bounds = Matter.Bounds.create([{ x: 5, y: 5 }, { x: 795, y: 595 }])
 
 var jump = 1.9;
 var maxSpeed = 4;
-var ground_acceleration = 0.35;
-var air_acceleration = 0.35;
-
-// Add the player to the world
-World.add(engine.world, player);
+var acceleration = 0.5;
 
 // Keep track of key presses
 var keys = [];
-document.body.addEventListener("keydown", function(e) {
+document.body.addEventListener('keydown', function(e) {
   keys[e.keyCode] = true;
 });
-document.body.addEventListener("keyup", function(e) {
+document.body.addEventListener('keyup', function(e) {
   keys[e.keyCode] = false;
 });
 
-//at the start of a colision for player
-Events.on(engine, "collisionStart", function(event) {
-  console.log(event.pairs[0]);
+// Set up general game
+var lives = [createPlayer({x: 600, y: 520})];
+World.add(engine.world, lives[0]);
+var ticks = 0;
+
+// Handle collisions
+Events.on(engine, 'collisionStart', function(event) {
   var pairs = event.pairs;
-  for (var i = 0, j = pairs.length; i != j; ++i) {
+  for (var i = 0; i < pairs.length; i++) {
     var pair = pairs[i];
-    console.log(pair.collision.normal);
-    if ((pair.bodyA === player || pair.bodyB === player) && Math.abs(pair.collision.normal.x) < 1) {
-      player.ground = true;
+
+    if (Math.abs(pair.collision.normal.x) < 1) {
+      var indexA = lives.indexOf(pair.bodyA);
+      var indexB = lives.indexOf(pair.bodyB);
+
+      // Need to examine pair.collision.normal.x and y
+      if (indexA >= 0 && pair.bodyA.position.y < pair.bodyB.position.y) {
+        lives[indexA].ground = true;
+      }
+      if (indexB >= 0  && pair.bodyB.position.y < pair.bodyA.position.y) {
+        lives[indexB].ground = true;
+      }
     }
   }
 });
-//ongoing checks for collisions for player
-Events.on(engine, "collisionActive", function(event) {
+
+Events.on(engine, 'collisionActive', function(event) {
   var pairs = event.pairs
-  for (var i = 0, j = pairs.length; i != j; ++i) {
+  for (var i = 0; i < pairs.length; i++) {
     var pair = pairs[i];
-    if ((pair.bodyA === player || pair.bodyB === player) && Math.abs(pair.collision.normal.x) < 1) {
-      player.ground = true;
+
+    if (Math.abs(pair.collision.normal.x) < 1) {
+      var indexA = lives.indexOf(pair.bodyA);
+      var indexB = lives.indexOf(pair.bodyB);
+
+      // Need to examine pair.collision.normal.x and y
+      if (indexA >= 0 && pair.bodyA.position.y < pair.bodyB.position.y) {
+        lives[indexA].ground = true;
+      }
+      if (indexB >= 0  && pair.bodyB.position.y < pair.bodyA.position.y) {
+        lives[indexB].ground = true;
+      }
     }
   }
 });
-//at the end of a colision for player
+
 Events.on(engine, 'collisionEnd', function(event) {
   var pairs = event.pairs;
-  for (var i = 0, j = pairs.length; i != j; ++i) {
+  for (var i = 0; i < pairs.length; i++) {
     var pair = pairs[i];
-    if (pair.bodyA === player || pair.bodyB === player) {
-      player.ground = false;
+    var indexA = lives.indexOf(pair.bodyA);
+    var indexB = lives.indexOf(pair.bodyB);
+
+    if (indexA >= 0) {
+      lives[indexA].ground = false;
+    }
+    if (indexB >= 0) {
+      lives[indexB].ground = false;
     }
   }
-})
+});
 
 //main engine update loop
-Events.on(engine, "beforeTick", function(event) {
-  if (keys[32]) {console.log(player)};
-  //jump
-  var total_force = {
-    x: 0,
-    y: 0,
+Events.on(engine, 'beforeTick', function(event) {
+  var playerActions = [];
+
+  // Handle recording moves for playback
+  if (keys[KEY_UP]) {
+    playerActions.push('jump');
+  }
+  if (keys[KEY_LEFT]) {
+    playerActions.push('left');
+  }
+  if (keys[KEY_RIGHT]) {
+    playerActions.push('right');
+  }
+  if (keys[KEY_CORPOREAL]) {
+    playerActions.push('corporeal');
   }
 
-  // Jumping
-  if (keys[38] && player.ground) {
-    total_force.y = -jump;
+  if (playerActions.length > 0) {
+    lives[0].moveList[ticks] = playerActions;
   }
-  // Moving left and right
-  if (keys[37] && Math.abs(player.velocity.x) < maxSpeed) {
-    total_force.x -= (player.ground ? ground_acceleration : air_acceleration);
-  } else if (keys[39] && Math.abs(player.velocity.x) < maxSpeed) {
-    total_force.x += (player.ground ? ground_acceleration : air_acceleration);
+
+  console.log(lives[0].velocity);
+
+  // Process stored moves
+  for (var i = lives.length - 1; i >=0; i--) {
+    var life = lives[i];
+    if (ticks in life.moveList) {
+      var total_force = {
+        x: 0,
+        y: 0,
+      }
+      if (life.moveList[ticks].indexOf('jump') >= 0 && life.ground) {
+        total_force.y = -jump;
+      }
+      if (life.moveList[ticks].indexOf('left') >= 0 && Math.abs(life.velocity.x) < maxSpeed) {
+        life.position.x -= acceleration;
+      }
+      if (life.moveList[ticks].indexOf('right') >= 0 && Math.abs(life.velocity.x) < maxSpeed) {
+        life.position.x += acceleration;
+      }
+
+      Body.setVelocity(life, { x: 0, y: life.velocity.y });
+
+      if (life.moveList[ticks].indexOf('corporeal') >= 0) {
+        life.render.fillStyle = (life.corporeal ? 'transparent' : '#ddd');
+        life.corporeal = !life.corporeal;
+        keys[KEY_CORPOREAL] = false;
+      }
+    }
   }
-  player.force = total_force;
+
+  if (keys[KEY_SUICIDE]) {
+    lives[0].active = false;
+    lives.unshift(createPlayer({x: 600, y: 520}));
+    World.add(engine.world, lives[0]);
+
+    // Reset everyone
+    for (var i = 0; i < lives.length; i++) {
+      lives[i].velocity = { x: 0, y: 0 };
+      lives[i].position = lives[i].spawn;
+    }
+
+    keys[KEY_SUICIDE] = false;
+
+    ticks = -1;
+  }
+
+  ticks += 1;
 });
 
 // run the engine
